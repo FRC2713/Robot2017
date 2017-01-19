17be95b47ab0c7de99c92ef239bd5b36ab46e23a
Conlan <conlanc@gmail.com>
2017-01-19 18:12:43 -0500

    Dynamically get the controller

--- src/main/java/org/usfirst/frc/team2713/OI.java
+++ src/main/java/org/usfirst/frc/team2713/OI.java
@@ -1,9 +1,14 @@
 package org.usfirst.frc.team2713;
 
+import edu.wpi.first.wpilibj.Joystick;
 import org.usfirst.frc.team2713.input.controller.XBoxController;
 
 public class OI {
 	private XBoxController xBoxController;
+
+	public OI(){
+		initControllers();
+	}
 
 	public XBoxController getController() {
 		return xBoxController;
@@ -15,6 +20,18 @@ public class OI {
 			default: return xBoxController;
 		}
 		//TODO Implement other controllers besides XBox
+	}
+
+	private void initControllers() {
+		for (int i = 0; i < 6; i++) {
+			Joystick test = new Joystick(i);
+			if (test.getName().equals(RobotMap.XBOX_NAME)) {
+				xBoxController = new XBoxController(i);
+			}
+		}
+		if (xBoxController == null) {
+			xBoxController = new XBoxController(RobotMap.BACKUP_XBOX_PORT);
+		}
 	}
 
 	public enum ControllerType {
--- src/main/java/org/usfirst/frc/team2713/RobotMap.java
+++ src/main/java/org/usfirst/frc/team2713/RobotMap.java
@@ -7,11 +7,14 @@ public class RobotMap {
 	public static final boolean DEBUG = false;
 	public static SendableChooser<Enum> OIDriveMode = new SendableChooser<>();
 
+	// Controller Ports
+	public static final int BACKUP_XBOX_PORT = 0;
+	public static final String XBOX_NAME = "Controller (XBOX 360 For Windows)"; // Used for dynamically finding the controller
+
 	// Talon Ports
 	public static final int TOP_RIGHT = 1;
 	public static final int TOP_LEFT = 3;
 	public static final int BOTTOM_RIGHT = 2;
 	public static final int BOTTOM_LEFT = 4;
-
 
 }
