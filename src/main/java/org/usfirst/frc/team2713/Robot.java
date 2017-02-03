d0eebf53f1797557c3f6201ca5051f1b13a443ab
Conlan <conlanc@gmail.com>
2017-02-03 18:54:05 -0500

    The Ryan Drive == Therian Drive

--- src/main/java/org/usfirst/frc/team2713/Robot.java
+++ src/main/java/org/usfirst/frc/team2713/Robot.java
@@ -59,7 +59,7 @@ public class Robot extends IterativeRobot {
 	private void initDash() {
 		RobotMap.OIDriveMode.addDefault("Tank Drive", DriveSubsystem.DriveModes.tank);
 		RobotMap.OIDriveMode.addObject("Arcade Drive", DriveSubsystem.DriveModes.arcade);
-		RobotMap.OIDriveMode.addObject("Ryan Drive", DriveSubsystem.DriveModes.ryanDrive);
+		RobotMap.OIDriveMode.addObject("Therian Drive", DriveSubsystem.DriveModes.ryanDrive);
 		RobotMap.OIDriveMode.addObject("Rocket League", DriveSubsystem.DriveModes.rocketleague);
 		SmartDashboard.putData("OI Mode", RobotMap.OIDriveMode);
 	}
