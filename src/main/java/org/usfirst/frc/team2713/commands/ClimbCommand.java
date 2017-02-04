8c7dca29dffbc4d0d05d46cf0c9966fa4d4990bf
Conlan <conlanc@gmail.com>
2017-02-03 19:46:25 -0500

    Make the climber be controlled by the Attack controller on a curve

--- src/main/java/org/usfirst/frc/team2713/commands/ClimbCommand.java
+++ src/main/java/org/usfirst/frc/team2713/commands/ClimbCommand.java
@@ -1,24 +1,40 @@
 package org.usfirst.frc.team2713.commands;
 
 import com.ctre.CANTalon;
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team2713.OI;
 import org.usfirst.frc.team2713.Robot;
 import org.usfirst.frc.team2713.subsystems.ClimbSubsystem;
 
 public class ClimbCommand extends Command {
 	private ClimbSubsystem climbSubsystem;
 	private CANTalon motor;
+	private double tally = 0;
 	
 	public ClimbCommand(ClimbSubsystem climbSubsystem) {
+		requires(climbSubsystem);
 		this.climbSubsystem = climbSubsystem;
 		this.motor = climbSubsystem.climbMotor;
 	}
 	
 	@Override
 	public void execute() {
-		double upSpeed = Robot.getOI().getController().getTriggerAxis(GenericHID.Hand.kLeft);
-		double downSpeed = -(Robot.getOI().getController().getTriggerAxis(GenericHID.Hand.kRight));
+		double increment = 0.01;
+		double upSpeed = 0;
+		double downSpeed = 0;
+		boolean isUp = Robot.getOI().getController(OI.ControllerType.fight).getRawButton(8); // Climb Up
+		boolean isDown = Robot.getOI().getController(OI.ControllerType.fight).getRawButton(4); // Climb Down
+		
+		if (isUp){
+			tally+=increment;
+			upSpeed = 0.1 + tally;
+		} else if (isDown){
+			tally+=increment;
+			downSpeed = 0.1 + tally;
+		} else {
+			tally = 0;
+		}
+		
 		if (upSpeed != 0 && downSpeed == 0) {
 			motor.set(upSpeed);
 		} else if (downSpeed != 0 && upSpeed == 0) {
