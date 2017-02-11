043a4074761fd6d869009737037fdc0d7df3b711
Connor Anderson <canderson@thaumavor.io>
2017-02-10 22:20:03 -0500

    Update Turn, remove IMU

--- src/main/java/org/usfirst/frc/team2713/commands/Turn.java
+++ src/main/java/org/usfirst/frc/team2713/commands/Turn.java
@@ -8,28 +8,25 @@ import org.usfirst.frc.team2713.Robot;
 
 public class Turn extends Command {
 	private Robot robot = Robot.getRobot();
-	
+
 	private PIDController pid;
 	private double angle;
-	
+
 	public Turn(double angle) {
 		requires(robot.getDrive());
-		
+
 		angle %= 360D;
 		this.angle = angle;
 	}
-	
+
 	@Override
 	protected void initialize() {
 		TalonOutput output = new TalonOutput(robot.getDrive().getLeftTalon(), robot.getDrive().getRightTalon());
-		pid = new PIDController(0.1, 0.0001, 0.01, robot.getDrive().getIMU(), output); // TODO: Tune PID
-		pid.setContinuous();
-		pid.setInputRange(-180D, 180D);
-		pid.setOutputRange(-1D, 1D);
-		pid.setAbsoluteTolerance(0.5D);
+		pid = new PIDController(0.1, 0, 0, robot.getDrive().getGyro(), output); // TODO: Tune PID
+		pid.setOutputRange(-0.25D, 0.25D);
+		pid.setAbsoluteTolerance(2D);
 
-		double newAngle = (robot.getDrive().getIMU().getAngle() + 180D + angle) % 360D - 180D;
-		pid.setSetpoint(newAngle);
+		pid.setSetpoint(robot.getDrive().getGyro().getAngle() + angle);
 		pid.enable();
 	}
 
@@ -37,21 +34,21 @@ public class Turn extends Command {
 	protected void end() {
 		pid.disable();
 	}
-	
+
 	@Override
 	protected boolean isFinished() {
 		return pid.onTarget();
 	}
-	
+
 	class TalonOutput implements PIDOutput {
 		private CANTalon left;
 		private CANTalon right;
-		
+
 		private TalonOutput(CANTalon left, CANTalon right) {
 			this.left = left;
 			this.right = right;
 		}
-		
+
 		@Override
 		public void pidWrite(double output) {
 			left.pidWrite(output);
--- src/main/java/org/usfirst/frc/team2713/subsystems/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc/team2713/subsystems/DriveSubsystem.java
@@ -1,9 +1,7 @@
 package org.usfirst.frc.team2713.subsystems;
 
-import com.analog.adis16448.frc.ADIS16448_IMU;
 import com.ctre.CANTalon;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team2713.Robot;
 import org.usfirst.frc.team2713.RobotMap;
@@ -15,35 +13,35 @@ public class DriveSubsystem extends Subsystem {
 	private CANTalon bottomLeft = new CANTalon(RobotMap.BOTTOM_LEFT);
 	private CANTalon bottomRight = new CANTalon(RobotMap.BOTTOM_RIGHT);
 	private RobotDrive roboDrive;
-	
-	private ADIS16448_IMU imu = new ADIS16448_IMU();
-	
+
+	private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
 	private boolean reversed = false;
-	
+
 	private boolean lastButtonState = false; // true = pressed, false = not pressed
 	private boolean isPressed = false;
-	
+
 	public DriveSubsystem() {
 		bottomLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
 		bottomLeft.set(RobotMap.TOP_LEFT);
-		
+
 		bottomRight.changeControlMode(CANTalon.TalonControlMode.Follower);
 		bottomRight.set(RobotMap.TOP_RIGHT);
-		
+
 		resetEncoders();
-		
+
 		roboDrive = new RobotDrive(topLeft, topRight);
 	}
-	
+
 	@Override
 	protected void initDefaultCommand() {
-		
+
 	}
-	
+
 	public void startTeleop() {
 		new OIDrive(this, roboDrive).start();
 	}
-	
+
 	public void tankDrive(double left, double right, double deadband, boolean checkReverseButton) {
 		int multiplier = getMultiplier(checkReverseButton);
 		roboDrive.tankDrive(getDeadband(left, deadband) * multiplier, getDeadband(right, deadband) * multiplier);
@@ -58,7 +56,7 @@ public class DriveSubsystem extends Subsystem {
 		checkReverser();
 		return checkReverse && reversed ? -1 : 1;
 	}
-	
+
 	private void checkReverser() {
 		// WPILib wants you to use a command for button triggers, but nah
 		if (Robot.getOI().getController().getBButton()) {
@@ -81,7 +79,7 @@ public class DriveSubsystem extends Subsystem {
 			}).start();
 		}
 	}
-	
+
 	public double getDeadband(double value, double deadband) {
 		int sign = (value > 0 ? 1 : -1); // Check if value is + or -
 		value = Math.abs(value); // Change value to Positive
@@ -91,32 +89,32 @@ public class DriveSubsystem extends Subsystem {
 			return (value - deadband) * sign; // returns value minus deadband
 		}
 	}
-	
+
 	public double getDeadband(double value) {
 		return getDeadband(value, 0.01);
 	}
-	
+
 	public void resetEncoders() {
-		// 1 (c=6) rotation, ~1440 ticks/rotation, ~18.8495in
+		// 1 (c=6) rotation, ~1440 ticks/rotation, ~18.8496in
 		topLeft.setEncPosition(0);
 		topRight.setEncPosition(0);
-		
+
 		topLeft.configEncoderCodesPerRev(1440); // As per https://tinyurl.com/jt5u24u
 		topRight.configEncoderCodesPerRev(1440);
 	}
-	
+
 	public CANTalon getLeftTalon() {
 		return topLeft;
 	}
-	
+
 	public CANTalon getRightTalon() {
 		return topRight;
 	}
-	
-	public ADIS16448_IMU getIMU() {
-		return imu;
+
+	public ADXRS450_Gyro getGyro() {
+		return gyro;
 	}
-	
+
 	public enum DriveModes {
 		tank, arcade, rocketleague, therian
 	}
