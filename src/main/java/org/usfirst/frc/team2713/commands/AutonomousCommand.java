1cef4e2715d0341bc9588570e37fb009ed5fdd61
Connor Anderson <canderson@thaumavor.io>
2017-02-19 21:30:38 -0500

    Use VisionAlign in autonomous

--- src/main/java/org/usfirst/frc/team2713/commands/AutonomousCommand.java
+++ src/main/java/org/usfirst/frc/team2713/commands/AutonomousCommand.java
@@ -8,11 +8,13 @@ public class AutonomousCommand extends CommandGroup {
 
 		switch (startingPosition) {
 			case 1:
-				addSequential(new Turn(60D));
+				addSequential(new Turn(60D, 2D));
+				addSequential(new VisionAlign());
 				addSequential(new MoveForward(57.8D));
 				break;
 			case 3:
-				addSequential(new Turn(-60D));
+				addSequential(new Turn(-60D, 2D));
+				addSequential(new VisionAlign());
 				addSequential(new MoveForward(57.8D));
 				break;
 		}
